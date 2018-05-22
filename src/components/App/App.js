4e42791df231598694308f45c0b786da77537486
juniormince <groundcontroltomajorbree@gmail.com>
2018-05-22 17:44:18 -0500

    add snackform component with action dispatch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      newSnack: '',
+    }
 
+  }
 
+  // tracking snack input field
+  handleChange = (event) => {
+    this.setState({
+      newSnack: event.target.value
+    });
+  }
 
-  
+  // quick, snack to the reducer mobile!
+  submitSnax = (event) => {
+    event.preventDefault(); //hey browser html we got it
+    const action = { type: 'ADD_SNACK', snack: this.state.newSnack }
+    this.props.dispatch(action);                 
+    this.setState({
+      newSnack: '',
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h3>IT'S AN INPUT FORM</h3>
+        <h3>IT'S FOR SNACKS</h3>
 
-
+        <form onSubmit={this.submitSnax}>
+          <input onChange={this.handleChange} value={this.state.newSnack} />
+          <input type="submit" value="snack it" />
+        </form>
 
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
+
+
+
+//reducer party here
+const firstReducer = (state = [], action) => {
+    if (action.type === 'ADD_SNACK')    {
+        console.log('first reducer czeck', action);
+        return [...state, 'czeck. add payload here'];
+    }
+    return state;
+}
+
+
+// creating store and c-c-c-combo any reducers
 const storeInstance = createStore(
     combineReducers({
-        firstReducer,
-
+        firstReducer
     }),
     applyMiddleware(logger)
 );
